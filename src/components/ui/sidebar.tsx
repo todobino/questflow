60890307f4947e0879c1313b1d78b981c0ee14ca
Brian Kimble <brian@stickbuilt.us>
2025-05-13 21:31:41 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <SidebarHeader>
    
    Relevant files:
    
        - https://6000-firebase-studio-1747165847076.cluster-hf4yr35cmnbd4vhbxvfvc6cp5q.cloudworkstations.dev/_next/static/chunks/src_a97e119d._.js
        - https://6000-firebase-studio-1747165847076.cluster-hf4yr35cmnbd4vhbxvfvc6cp5q.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    Remove the padding around this image I want it to go to the edges

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -359,7 +359,7 @@ const SidebarHeader = React.forwardRef<
     <div
       ref={ref}
       data-sidebar="header"
-      className={cn("flex flex-col gap-2 p-2", className)}
+      className={cn("flex flex-col gap-2", className)} // Removed p-2
       {...props}
     />
   )
@@ -762,4 +762,3 @@ export {
   SidebarTrigger,
   useSidebar,
 }
-
